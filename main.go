3558a26ea765c4faa2c6a04826597493a72472bf
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2025-06-01 21:48:44 +1000

    started file parsing process

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/BrendanWallaceNash98/golang-1brc-io/logger"
+	"github.com/BrendanWallaceNash98/golang-1brc-io/models"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	data, err := os.ReadFile(`/Users/brendanwallace-nash/OneBillionRowChallenge/data/measurements.txt`)
+	logger.PanicError(err)
+	var wso models.WeatherStations
+	for d := range data {
+		dStr := string(d)
+		fmt.Println(dStr)
+		dList := strings.Split(dStr, `;`)
+		num, err := strconv.ParseFloat(dList[1], 64)
+		logger.PanicError(err)
+		wso.AddWeatherStation(dList[0], num)
+	}
 }
