6799a77396708c8b4eb56b6450c7ddbc2c5802d2
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2025-06-21 21:08:14 +1000

    we go agian

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/BrendanWallaceNash98/golang-1brc-io/logger"
 	"github.com/BrendanWallaceNash98/golang-1brc-io/models"
 )
 
-func sepentialRead() {
+func main() {
 	file, err := os.Open(`/Users/brendanwallace-nash/OneBillionRowChallenge/data/measurements.txt`)
 	logger.PanicError(err)
 	defer func(file *os.File) {
@@ -43,80 +42,6 @@ func sepentialRead() {
 			fmt.Printf("Processed %d lines\n\n", lineCount)
 		}
 	}
-	outPutFile, err := os.Create("/Users/brendanwallace-nash/OneBillionRowChallenge/data/output.txt")
-	logger.LogError(err)
-	defer func() {
-		if err := outPutFile.Close(); err != nil {
-			logger.PanicError(err)
-		}
-	}()
-
-	for _, weatherStation := range wso.WeatherStationsNameSorted {
-		station := wso.WeatherStationsMap[weatherStation]
-		station.CalAverageTemp()
-		stationText := fmt.Sprintf("%s=%v/%v/%v\n", station.Name, station.Min, station.Avg, station.Max)
-		if _, err := outPutFile.WriteString(stationText); err != nil {
-			logger.LogError(err)
-		}
-	}
-
-	if err := outPutFile.Sync(); err != nil {
-		logger.PanicError(err)
-	}
-
-	logger.PanicError(scanner.Err())
-}
-
-func main() {
-	file, err := os.Open(`/Users/brendanwallace-nash/OneBillionRowChallenge/data/measurements.txt`)
-	logger.PanicError(err)
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			logger.LogError(err)
-		}
-	}(file)
-
-	scanner := bufio.NewScanner(file)
-	wso := models.IntialiseWeatherStation()
-    
-    type result struct {
-        stationName string,
-        temp float64
-    }
-
-    resultChannel := make(chan result, 10000)
-
-    var mu sync.Mutex
-	lineCount := 0
-	var wg sync.WaitGroup
-
-	concurrentLineProcess := func(line string, wso models.WeatherStations) {
-		defer wg.Done()
-		semiColonPos := strings.Index(line, ";")
-		if semiColonPos == -1 {
-			return
-		}
-		stationName := line[:semiColonPos]
-		tempStr := line[semiColonPos+1:]
-		temp, err := strconv.ParseFloat(tempStr, 64)
-		logger.LogError(err)
-		mu.Lock()
-		wso.AddWeatherStation(stationName, temp)
-
-		lineCount++
-		if lineCount%10000000 == 0 {
-			fmt.Printf("Processed %d lines\n\n", lineCount)
-		}
-		mu.Unlock()
-	}
-	for scanner.Scan() {
-		line := scanner.Text()
-		wg.Add(1)
-		go concurrentLineProcess(line, wso)
-	}
-	wg.Wait()
-
 	outPutFile, err := os.Create("/Users/brendanwallace-nash/OneBillionRowChallenge/data/output.txt")
 	logger.LogError(err)
 	defer func() {
