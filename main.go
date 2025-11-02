b6e7c6c0cc0b56399adf70716b4e5f80a03d8383
BrendanWallaceNash98 <brendan.wallacenash@connect.qut.edu.au>
2025-11-02 22:15:16 +1000

    breaking up processing to use channels and read file as chuncks

--- main.go
+++ main.go
@@ -4,23 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"runtime"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/BrendanWallaceNash98/golang-1brc-io/logger"
 	"github.com/BrendanWallaceNash98/golang-1brc-io/models"
 )
 
-func main() {
-	file, err := os.Open(`/Users/brendanwallace-nash/OneBillionRowChallenge/data/measurements.txt`)
-	logger.PanicError(err)
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			logger.LogError(err)
-		}
-	}(file)
+func processFile(file *os.File, chunkStart int64, chunkEnd int64, waitgroup *sync.WaitGroup) {
 	scanner := bufio.NewScanner(file)
 	wso := models.IntialiseWeatherStation()
 
@@ -32,12 +26,53 @@ func main() {
 			continue
 		}
 		stationName := line[:semiColonPos]
-		tempStr := line[semiColonPos+1:]
+		tempStr := line[semiColonPos+1 : len(line)-1]
 		temp, err := strconv.ParseFloat(tempStr, 64)
-		logger.LogError(err)
+		if err != nil {
+			logger.LogError(err)
+		}
 		wso.AddWeatherStation(stationName, temp)
 
 	}
+}
+
+func main() {
+	filename := `/Users/brendanwallace-nash/OneBillionRowChallenge/data/measurements.txt`
+	file, err := os.Open(filename)
+	logger.PanicError(err)
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			logger.LogError(err)
+		}
+	}(file)
+
+	numWorker := runtime.NumCPU()
+
+	fileInfo, _ := os.Stat(filename)
+	fileSize := fileInfo.Size()
+	chunkSize := fileSize / int64(numWorker)
+
+	results := make(chan map[string]*models.WeatherStation, numWorker)
+	var wg sync.WaitGroup
+
+	for i := 0; i < numWorker; i++ {
+		wg.Add(1)
+		start := int64(i) * chunkSize
+		end := start + chunkSize
+		if i == numWorker-1 {
+			end = fileSize
+		}
+
+		go processFile(file, start, end, &wg)
+
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
 	outPutFile, err := os.Create("/Users/brendanwallace-nash/OneBillionRowChallenge/data/output.txt")
 	logger.LogError(err)
 	defer func() {
